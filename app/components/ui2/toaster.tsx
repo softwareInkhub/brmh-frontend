c773d59cc13dae9be9a4acf9512c79b52e63b4f6
softwareInkhub <software.inkhub@gmail.com>
2025-03-28 14:20:15 +0530

    updates

--- app/components/ui2/toaster.tsx
+++ app/components/ui2/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/app/components/ui2/toast"
+import { useToast } from "@/app/components/ui2/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
